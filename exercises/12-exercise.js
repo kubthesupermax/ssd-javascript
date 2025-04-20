f204b2e3d13bdd10aafda4fe8ba385e0f5544430
Franklin Kuba <franklinkuba7@gmail.com>
2025-04-19 22:19:37 -0500

    Add section 12 exercises

--- exercises/12-exercise.js
+++ exercises/12-exercise.js
@@ -1,4 +1,5 @@
-//12a
+/*
+//12a 
 const add = function () {
   console.log(2, 3);
 };
@@ -25,12 +26,39 @@ buttonElement.addEventListener("click", () => {
   }, 1000);
 });
 
-//12e
+//12e and 12f
 const buttonElement2 = document.querySelector(".js-add-to-cart-button");
 const added = document.querySelector(".js-added");
 buttonElement2.addEventListener("click", () => {
   added.innerHTML = "Added!";
-  setTimeout(() => {
+  const timeout = setTimeout(() => {
     added.innerHTML = "";
   }, 2000);
+
+  clearTimeout(timeout);
 });
+
+// 12g
+let messages = 2;
+setInterval(function () {
+  if (document.title === "App") {
+    document.title = `(${messages}) New messages`;
+  } else {
+    document.title = "App";
+  }
+}, 1000);
+
+// 12h and 12i
+const buttonElement3 = document.querySelector(".js-add-button");
+buttonElement3.addEventListener("click", () => {
+  messages = messages + 1;
+  document.title = `(${messages}) New messages`;
+});
+
+const buttonElement4 = document.querySelector(".js-remove-button");
+buttonElement4.addEventListener("click", () => {
+  messages > 0 ? (messages = messages - 1) : (messages = 0);
+  document.title = `(${messages}) New messages`;
+});
+
+*/
